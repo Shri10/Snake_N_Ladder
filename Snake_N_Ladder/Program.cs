ba3db83a1217e83f22322fc9f5977082bb6ae8be
Shri10 <pethe.shriraj@gmail.com>
2023-05-11 11:25:59 +0530

    first commit

---
+++ Snake_N_Ladder/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Snake_N_Ladder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
