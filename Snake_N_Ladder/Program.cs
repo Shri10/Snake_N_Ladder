a29665743a07bc2b4aab8d3ff2dcae04acdb885c
Shri10 <pethe.shriraj@gmail.com>
2023-05-11 14:53:01 +0530

    [Shri10] Created UC1

--- Snake_N_Ladder/Program.cs
+++ Snake_N_Ladder/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Snake_N_Ladder
+﻿using System;
+
+namespace Snake_N_Ladder
 {
     internal class Program
     {
@@ -7,8 +9,19 @@
             Console.WriteLine("Welcome to ...");
             Console.WriteLine("\r\n   ____ _  __ ___    __ __ ____ ____  ____      __    ___    ___   ___   ____ ___ \r\n  / __// |/ // _ |  / //_// __// __/ / __/___  / /   / _ |  / _ \\ / _ \\ / __// _ \\\r\n _\\ \\ /    // __ | / ,<  / _/ _\\ \\   > _/_ _/ / /__ / __ | / // // // // _/ / , _/\r\n/___//_/|_//_/ |_|/_/|_|/___//___/  |_____/  /____//_/ |_|/____//____//___//_/|_| \r\n                                                                                  \r\n");
             Console.WriteLine("┌┬┐┌─┐┌┬┐┌─┐  ┌┐ ┬ ┬  ╔═╗╦ ╦╦═╗╦╦═╗╔═╗ ╦\r\n│││├─┤ ││├┤   ├┴┐└┬┘  ╚═╗╠═╣╠╦╝║╠╦╝╠═╣ ║\r\n┴ ┴┴ ┴─┴┘└─┘  └─┘ ┴   ╚═╝╩ ╩╩╚═╩╩╚═╩ ╩╚╝");
-            int position = 0; // player's starting position
-            int numberOfRolls = 0; // number of times the dice was rolled
+            
+            Console.WriteLine("Press anything to start");
+            Console.ReadLine();
+
+            int Pl1_position = 0; // player's starting position
+            int Pl1_numberOfRolls = 0; // number of times the dice was rolled
+            Random random = new Random();
+
+            int Pl1_diceRoll = random.Next(1, 7);
+            Pl1_numberOfRolls++;
+
+            Console.WriteLine("Dice for Player 1 Rolled: " + Pl1_diceRoll);
+
 
         }
     }
